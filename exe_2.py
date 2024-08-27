13992bb8b861924bb4df5daaeac007b9e686c748
obboye <abless024@gmail.com>
2024-08-27 09:01:54 +0000

    Exercise 2 code edited

--- exe_2.py
+++ exe_2.py
@@ -8,10 +8,10 @@
 num1 = input('Enter first number here: ')
 num2 = input('Enter second number here: ')
 
-print(f"{num1} + {num2} = {int(num1) + int(num2)}")
-print(f"{num1} - {num2} = {int(num1) - int(num2)}")
-print(f"{num1} x {num2} = {int(num1) * int(num2)}")
-print(f"{num1} / {num2} = {int(num1) // int(num2)}")
+print(f"Sum: {num1} + {num2} = {int(num1) + int(num2)}")
+print(f"Difference: {num1} - {num2} = {int(num1) - int(num2)}")
+print(f"Product: {num1} x {num2} = {int(num1) * int(num2)}")
+print(f"Quotient: {num1} / {num2} = {int(num1) / int(num2)}")
 
 
 print('Done')
