ff2a06ade32d6b14a2fa2f5a3ab6d280a3469055
obboye <abless024@gmail.com>
2024-08-27 08:56:37 +0000

    Exercise 2 completed

---
+++ exe_2.py
@@ -0,0 +1,17 @@
+
+#  TODO: 
+# Exercise: Write a program that takes two numbers as 
+# input from the user and prints their sum, difference, 
+# product, and quotient.
+
+
+num1 = input('Enter first number here: ')
+num2 = input('Enter second number here: ')
+
+print(f"{num1} + {num2} = {int(num1) + int(num2)}")
+print(f"{num1} - {num2} = {int(num1) - int(num2)}")
+print(f"{num1} x {num2} = {int(num1) * int(num2)}")
+print(f"{num1} / {num2} = {int(num1) // int(num2)}")
+
+
+print('Done')
