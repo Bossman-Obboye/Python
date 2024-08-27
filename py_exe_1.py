5d8bc58720554b29adf536136fd68eec29fa1bab
obboye <abless024@gmail.com>
2024-08-27 07:07:56 +0000

    First exercise

---
+++ py_exe_1.py
@@ -0,0 +1,31 @@
+
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Try programiz.pro")
+
+doctor = 'Health'
+
+teacher = 'Education'
+
+TRADER = 'Sales'
+
+print((TRADER + '..' + doctor)[9])
+print((TRADER + '..' + doctor))
+
+
+name = input('Enter your name: ')
+age = input('How old are you: ')
+
+print(type(name))
+# def :
+print('Hello '+ name, 'you are', age, 'years old.')
+
+
+# Ask the user for their name
+name = input("Enter your name: ")
+
+# Ask the user for their age
+age = input("Enter your age: ")
+
+# Print a personalized message
+print(f"Hello, {name}! You are {age} years old.")
