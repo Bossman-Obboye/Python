7889beac336bf6c43f66843a31acee76210cd991
obboye <abless024@gmail.com>
2024-08-31 14:07:44 +0000

    Exercise 5 done

---
+++ exe_5.py
@@ -0,0 +1,7 @@
+# Exercise: Write a program that converts a temperature given in Celsius 
+# to Fahrenheit. The formula for conversion is:
+# Fahrenheit=Celsius×5/9​+32
+
+tempInCelsius = input("Enter temperature in Celsius: ")
+tempInFehrenheit = int(tempInCelsius) * (5/9) + 32
+print(f"Temperature in Fehrenheit is: {tempInFehrenheit}")
