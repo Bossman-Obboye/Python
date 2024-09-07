0b249f2c4d9ad8eadf35aaf77bc0a81c50a6714e
obboye <abless024@gmail.com>
2024-09-07 11:50:12 +0000

    Exercise_6 completed

---
+++ exe_6.py
@@ -0,0 +1,22 @@
+# Exercise: Write a program that takes a list of five names as 
+# input and then prints each name in uppercase.
+
+# List to store the names
+list_of_names = []
+
+# Utils -- Positions
+list_of_state = ('first', 'second', 'third', 'forth', 'last')
+
+# By iterating within the range of 5, accept, convert and assign 
+# all the names to 'list_of_names'
+for i in range(5) :
+    # Accept name
+    name = input(f"Enter {list_of_state[i]} person's name here: ")
+    
+    # Convert to uppercase
+    name = name.upper()
+    
+    # Assign name
+    list_of_names.append(name)
+
+print(list_of_names)
