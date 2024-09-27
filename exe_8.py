e8801c83bcc4bd3450adbe6ecf7f782eb9f9c389
Bossman Obboye <abless024@gmail.com>
2024-09-27 01:11:24 -0700

    Exercise 8 improved

--- exe_8.py
+++ exe_8.py
@@ -3,9 +3,17 @@
 
 import re
 
+# Accepting Input from the User
 dataFromUser = input("Enter 'String' to be checked: ")
 
+# Pattern of Vowels to use for the check
 pattern = r'[aeiouAEIOU]'
+
+# Create a list of the vowels in the user's input
 vowelsInDataFromUser = re.findall(pattern, dataFromUser)
 
-print(f"The number of vowels in the 'String' you provided is: {len(vowelsInDataFromUser)}")
+# Find the number of vowels and assign it to "count"
+count = len(vowelsInDataFromUser)
+
+# Print the result
+print(f"The number of vowels in the 'String' you provided is: {count}")
---
+++ exe_9.py
@@ -0,0 +1,9 @@
+# Exercise 9: Simple Guessing Game
+
+# Objective: Learn to use loops and conditional statements
+# to create a basic game.
+
+# Exercise: Write a program that generates a random number
+# between 1 and 10 and asks the user to guess the number. 
+# The program should provide feedback and continue to prompt 
+# the user until the correct number is guessed.
